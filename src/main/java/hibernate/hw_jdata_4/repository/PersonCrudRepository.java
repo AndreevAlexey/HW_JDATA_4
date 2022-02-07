cabbcc4ff7179dd9c5d7cdc49401b24ad12fef5e
Andreev Alexey <andreev-j@yandex.ru>
2022-02-07 20:10:31 +0700

    added custom query

--- src/main/java/hibernate/hw_jdata_4/repository/PersonCrudRepository.java
+++ src/main/java/hibernate/hw_jdata_4/repository/PersonCrudRepository.java
@@ -3,8 +3,6 @@ package hibernate.hw_jdata_4.repository;
 import hibernate.hw_jdata_4.person.Person;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,12 +12,9 @@ import java.util.List;
 public interface PersonCrudRepository extends JpaRepository<Person, Long> {
 
     // полчить список личностей по городу
-    @Query(value = "select p from Person p where upper(p.city.name) = upper(:city)")
-    List<Person> findAllByCityNameIgnoreCase(@Param("city") String name);
+    List<Person> findAllByCityNameIgnoreCase(String name);
     // полчить список личностей младше возраста
-    @Query(value = "select p from Person p where p.id.age < :age")
-    List<Person> findByIdAgeLessThan(@Param("age") int age, Sort sort);
+    List<Person> findByIdAgeLessThan(int age, Sort sort);
     // полчить список личностей по имени и фамилии
-    @Query(value = "select p from Person p where p.id.name = :name and p.id.surname = :surname")
-    List<Person> findByIdNameAndIdSurnameIgnoreCase(@Param("name") String name, @Param("surname") String surname);
+    List<Person> findByIdNameAndIdSurnameIgnoreCase(String name, String surname);
 }
