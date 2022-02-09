3fc3c436190413b026785f51ff64fac39b7d4cd6
Andreev Alexey <andreev-j@yandex.ru>
2022-02-09 13:53:55 +0700

    added security methods

--- src/main/java/hibernate/hw_jdata_4/config/SecurityConfig.java
+++ src/main/java/hibernate/hw_jdata_4/config/SecurityConfig.java
@@ -2,26 +2,30 @@ package hibernate.hw_jdata_4.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication().withUser("user").password(encoder.encode("user")).roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(encoder.encode("user1")).roles("READ")
+                .and()
+                .withUser("user2").password(encoder.encode("user2")).roles("WRITE")
+                .and()
+                .withUser("user3").password(encoder.encode("user3")).roles("DELETE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .and()
-                .authorizeRequests()
-                .antMatchers("/persons/by-city").authenticated()
-                .and()
-                .authorizeRequests().anyRequest().permitAll();
+                .authorizeRequests().anyRequest().authenticated();
     }
 }
--- src/main/java/hibernate/hw_jdata_4/controller/PersonController.java
+++ src/main/java/hibernate/hw_jdata_4/controller/PersonController.java
@@ -4,11 +4,16 @@ import hibernate.hw_jdata_4.person.Person;
 import hibernate.hw_jdata_4.service.PersonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -20,24 +25,41 @@ public class PersonController {
     }
 
     @GetMapping("/persons/by-city")
+    @Secured("ROLE_READ")
     public List<Person> getPersonsByCity(@RequestParam("city") String city) {
         return personService.getPersonsByCity(city);
     }
 
+
     @GetMapping("/persons/by-age-lower")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> getPersonsByAgeLower(@RequestParam("age") int age) {
         return personService.getPersonsByAgeLower(age);
     }
 
     @GetMapping("/persons/by-name-surname")
+    @PreAuthorize("hasRole('ROLE_DELETE')")
     public List<Person> getPersonsByNameAndSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return personService.getPersonsByNameAndSurname(name, surname);
     }
 
+    @GetMapping("/persons/userinfo")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String getUserInfo(@RequestParam("username") String username) {
+        return SecurityContextHolder.getContext().getAuthentication().toString();
+    }
+
     @ExceptionHandler
-    ResponseEntity<String> handlerRuntimeException(RuntimeException exp) {
+    ResponseEntity<String> handlerAccessDeniedException(AccessDeniedException exp) {
+        return new ResponseEntity<>(exp.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<String> handlerRuntimeException(Exception exp) {
         exp.printStackTrace();
         return new ResponseEntity<>("Ошибка обработки запроса", HttpStatus.SERVICE_UNAVAILABLE);
     }
+
+
 
 }
